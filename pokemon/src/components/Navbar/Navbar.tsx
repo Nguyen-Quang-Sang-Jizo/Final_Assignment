e7c5c8f5d09a1e8c1d06d76b7afef48852008db5
Nguyen-Quang-Sang-Jizo <117348347+Nguyen-Quang-Sang-Jizo@users.noreply.github.com>
2023-12-15 00:44:46 +0000

    update

--- pokemon/src/components/Navbar/Navbar.tsx
+++ pokemon/src/components/Navbar/Navbar.tsx
@@ -142,7 +142,7 @@ const Navbar = ({ refreshComponent, setRefreshComponent }: Props) => {
             )}
           </nav>
           <div style={{ marginLeft: 'auto', display: 'flex', alignItems: 'center' }}>
-            <Typography>{username}</Typography>
+            <Typography sx={{fontFamily:'Roboto, sans-serif'}}>{username}</Typography>
             <Button
               sx={{ color: 'white' }}
               aria-controls="simple-menu"
